5602ffc66d833d4adc29f6873387d02ee467ebf8
Noel <ndalfaro333@gmail.com>
2024-02-16 01:39:30 -0600

    added shadcn table component to see how data looks formatted

--- app/components/players.tsx
+++ app/components/players.tsx
@@ -1,17 +1,63 @@
 import { fetchPlayerData } from "@/app/lib/data";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+type Player = {
+  id: number;
+  name: string;
+  stats: {
+    apg: number;
+    ppg: number;
+    rpg: number;
+    plusMinus: number;
+    ciceroScore: number;
+  };
+  position: string;
+  hometown: string;
+};
 
 async function Players() {
   const data = await fetchPlayerData();
 
+  // console.log(data);
   return (
     <>
       <h1>Player's in database</h1>
-      {data.map((player: any) => (
-        <h1 className="flex text-xl" key={player.id}>
-          {player.name} -
-          <p className="font-bold"> Cicero Score: {player.stats.ciceroScore}</p>
-        </h1>
-      ))}
+      <Table>
+        <TableCaption>A list player's and their cicero scores</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Position</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>PPG</TableHead>
+            <TableHead>APG</TableHead>
+            <TableHead>Hometown</TableHead>
+            <TableHead className="text-right">Score</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((player: Player) => (
+            <TableRow key={player.id}>
+              <TableCell className="font-medium">{player.position}</TableCell>
+              <TableCell>{player.name}</TableCell>
+              <TableCell>{player.stats.ppg}</TableCell>
+
+              <TableCell>{player.stats.apg}</TableCell>
+              <TableCell>{player.hometown}</TableCell>
+              <TableCell className="text-right">
+                {player.stats.ciceroScore}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </>
   );
 }
