c90beeedb28a77f0804e69c6c459e06ae1750cfd
Noel <ndalfaro333@gmail.com>
2024-03-25 15:03:27 -0500

    testing style for shadcn table component

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,8 +4,11 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <nav className="flex h-auto  w-full max-w-5xl items-center justify-between pb-3 pt-3">
-      <p className="text-2xl font-extrabold">Prospect Portfolio</p>
+    <nav className="flex h-auto  w-full max-w-5xl items-center justify-between">
+      <Link href="/">
+        <p className="text-2xl font-extrabold">Prospect Portfolio</p>
+      </Link>
+
       <div className="flex w-1/2 items-center justify-between rounded-lg border border-solid p-4">
         <Link href="/">Home</Link>
         <Link href="/explore">Explore</Link>
