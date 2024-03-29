418bb67aa8d1f399214bd6e9485d106ca3deffcb
Noel <ndalfaro333@gmail.com>
2024-03-29 04:22:33 -0500

    commented out auth logic on home page to test failed deployment

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default async function Home() {
-  const { getUser, isAuthenticated } = getKindeServerSession();
-  const user = await getUser();
+  // const { getUser, isAuthenticated } = getKindeServerSession();
+  // const user = await getUser();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
