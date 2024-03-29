d942bb8fb91f8cb0425eaa1a423f40487d8b908b
Noel <ndalfaro333@gmail.com>
2024-03-29 04:23:36 -0500

    commented out auth logic on home page to test failed deployment

--- app/api/auth/success/route.ts
+++ app/api/auth/success/route.ts
@@ -1,35 +1,35 @@
-// import { db } from "@/db";
-// import { users } from "@/db/schema/users";
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-// import { eq } from "drizzle-orm";
-// import { NextResponse } from "next/server";
-// import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { users } from "@/db/schema/users";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 
-// export async function GET(req: Request) {
-//   // check if user exists
-//   const { getUser } = getKindeServerSession();
-//   const user = await getUser();
-//   // const { orgCode } = await getOrganization();
+export async function GET(req: Request) {
+  // check if user exists
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  // const { orgCode } = await getOrganization();
 
-//   if (!user || !user.id || !user.email) {
-//     return NextResponse.json(
-//       { error: "User not found or missing ID" },
-//       { status: 401 },
-//     ); // Adjust error handling as needed
-//   }
-//   const dbUser = await db.select().from(users).where(eq(users.id, user.id));
-//   // if (dbUser) console.log("we got the user");
-//   console.log(dbUser);
+  if (!user || !user.id || !user.email) {
+    return NextResponse.json(
+      { error: "User not found or missing ID" },
+      { status: 401 },
+    ); // Adjust error handling as needed
+  }
+  const dbUser = await db.select().from(users).where(eq(users.id, user.id));
+  // if (dbUser) console.log("we got the user");
+  console.log(dbUser);
 
-//   if (!dbUser) {
-//     await db.insert(users).values({
-//       id: user.id,
-//       firstName: user.given_name,
-//       lastName: user.family_name,
-//       email: user.email,
-//       image: user.picture,
-//     });
-//   }
-//   console.log("route successfully ran");
-//   return redirect("http://localhost:3000/dashboard");
-// }
+  if (!dbUser) {
+    await db.insert(users).values({
+      id: user.id,
+      firstName: user.given_name,
+      lastName: user.family_name,
+      email: user.email,
+      image: user.picture,
+    });
+  }
+  console.log("route successfully ran");
+  return redirect("http://localhost:3000/dashboard");
+}
