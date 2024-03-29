f7a205a03a359afbcc885a6925661d407b26dd31
Noel <ndalfaro333@gmail.com>
2024-03-29 04:14:48 -0500

    changed env config to env.local on drizzle config file

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { migrate } from "drizzle-orm/neon-http/migrator";
 import { config } from "dotenv";
-config({ path: ".env" });
+config({ path: ".env.local" });
 
 const databaseUrl: string = process.env.DRIZZLE_DATABASE_URL as string;
 
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "drizzle-kit";
+import { config } from "dotenv";
+config({ path: ".env.local" });
 
 // const databaseUrl: string = process.env.POSTGRES_URL as string;
 const databaseUrl: string = process.env.DRIZZLE_DATABASE_URL as string;
