b8bccd7870681175d619896e6ff169a936fa834f
Noel <ndalfaro333@gmail.com>
2024-03-30 02:16:18 -0500

    edited logout link in logout button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,13 +9,17 @@ export default async function Page() {
   // console.log(await getUser());
   // console.log(await isAuthenticated());
   const user = await getUser();
+  const redirectURL =
+    process.env.NODE_ENV === "production"
+      ? "https://cicero-coral.vercel.app/"
+      : "http://localhost:3000/";
 
   // if (!session?.user) redirect("/api/auth/signin?callbackUrl=/dashboard");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Dashboard for {user?.given_name}
       <LogoutLink
-        postLogoutRedirectURL="/"
+        postLogoutRedirectURL={redirectURL}
         className="mt-8 inline-block text-blue-500 underline"
       >
         <button>Logout</button>
