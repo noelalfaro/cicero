5ba4d7ff9e2866670cf0a9bee88f999d25ed2e95
Noel <ndalfaro333@gmail.com>
2024-09-17 02:30:20 -0500

    added auth check to dashboard to display user info in card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,44 +3,51 @@ import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server';
 import { Link } from 'next-view-transitions';
 import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardContent } from '@/components/ui/card';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
   const { getUser, isAuthenticated } = getKindeServerSession();
   const user = await getUser();
 
   return (
-    <div className="flex w-full flex-col items-center justify-start">
-      <Card className="w-full">
-        <CardHeader className="text-4xl font-bold">
-          Dashboard For...{' '}
-          <Link href={`/${user!.username}`}>
-            <p className="font-mono hover:text-primary hover:underline">
-              @{user?.username}
-            </p>
-          </Link>
-        </CardHeader>
-        <CardContent>
-          <div className="text-balance">
-            Welcome to Prospect Portfolio! <br /> We're still under active
-            development. <br /> I welcome you to navigate to the different pages
-            in the nav above or click on a link below to explore what a player's
-            individual page currently looks like.
-          </div>
-          <div className="flex w-full justify-center">
-            <Link href={'/players/2544'}>
-              <Button variant={'link'}>Link to Lebron's Page</Button>
-            </Link>
-            <Link href={'/players/201142'}>
-              <Button variant={'link'}>Link to Durant's Page</Button>
-            </Link>
-            <LogoutLink>
-              <Button variant={'link'} className="text-red-600">
-                Logout
-              </Button>
-            </LogoutLink>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
+    <>
+      {!isAuthenticated() ? (
+        redirect(`/`)
+      ) : (
+        <div className="flex w-full flex-col items-center justify-start">
+          <Card className="w-full">
+            <CardHeader className="text-4xl font-bold">
+              Dashboard For...{' '}
+              <Link href={`/${user?.username}`}>
+                <p className="font-mono hover:text-primary hover:underline">
+                  @{user?.username}
+                </p>
+              </Link>
+            </CardHeader>
+            <CardContent>
+              <div className="text-balance">
+                Welcome to Prospect Portfolio! <br /> We're still under active
+                development. <br /> I welcome you to navigate to the different
+                pages in the nav above or click on a link below to explore what
+                a player's individual page currently looks like.
+              </div>
+              <div className="flex w-full justify-center">
+                <Link href={'/players/2544'}>
+                  <Button variant={'link'}>Link to Lebron's Page</Button>
+                </Link>
+                <Link href={'/players/201142'}>
+                  <Button variant={'link'}>Link to Durant's Page</Button>
+                </Link>
+                <LogoutLink>
+                  <Button variant={'link'} className="text-red-600">
+                    Logout
+                  </Button>
+                </LogoutLink>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      )}
+    </>
   );
 }
