cd0230e75df0dfa7431cac7e4744e92b789a38a4
Noel <ndalfaro333@gmail.com>
2024-11-14 21:21:42 -0600

    changed git config core.ignorecase false to test if i changed file name uppercase/lowercase

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // webpack: (config) => {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '@': path.resolve(__dirname, './'),
+  //   };
+
+  //   return config;
+  // },
   images: {
     remotePatterns: [
       {
