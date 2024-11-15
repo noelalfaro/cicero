a5aa6c84b45fe8fdf28549d7cdf3217dae5799d9
Noel <ndalfaro333@gmail.com>
2024-11-14 21:28:10 -0600

    updated next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,51 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // webpack: (config) => {
-  //   config.resolve.alias = {
-  //     ...config.resolve.alias,
-  //     '@': path.resolve(__dirname, './'),
-  //   };
+// const nextConfig = {
+//   // webpack: (config) => {
+//   //   config.resolve.alias = {
+//   //     ...config.resolve.alias,
+//   //     '@': path.resolve(__dirname, './'),
+//   //   };
 
-  //   return config;
-  // },
+//   //   return config;
+//   // },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'lh3.googleusercontent.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'avatars.githubusercontent.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'ak-static.cms.nba.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'i.pinimg.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'utfs.io',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+import path from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -33,5 +71,4 @@ const nextConfig = {
     ],
   },
 };
-
 export default nextConfig;
