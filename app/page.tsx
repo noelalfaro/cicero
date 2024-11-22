bfa0ede54ff72507404c5997e4a9520fffb30637
Noel <ndalfaro333@gmail.com>
2024-11-22 02:29:27 -0600

    changed landing page styling

--- app/page.tsx
+++ app/page.tsx
@@ -14,23 +14,29 @@ export default async function Home() {
       {/* Container for the page */}
       <div className="flex min-h-screen flex-col">
         {/* Hero section */}
-        <section className="flex min-h-screen w-full flex-col items-start justify-end gap-2 pb-[2svh] md:justify-center md:gap-2 md:pb-0">
+        <section className="flex min-h-screen w-full flex-col items-start justify-end gap-4 pb-10 text-left md:gap-2 md:pb-0">
           <div className="flex w-fit flex-col">
             <h1 className={`w-full text-5xl font-bold md:text-8xl lg:text-9xl`}>
               PROSPECT <br /> PORTFOLIO
             </h1>
-            <h2 className="lg:text-2xl">
+
+            <h2 className="text-lg font-semibold lg:text-2xl">
               You think they're the next big thing? <br /> Prove it.
             </h2>
           </div>
 
-          <div className="flex w-full justify-start gap-1 self-end text-center md:w-fit md:justify-end md:gap-2 lg:grid-cols-4 lg:text-left">
+          <div className="flex w-full justify-start gap-2 self-end text-center md:w-fit md:justify-end md:gap-2 lg:grid-cols-4 lg:text-left">
             <Link href={'/login'} className="w-1/2">
-              <Button className="w-full p-6 text-lg">Log In</Button>
+              <Button className="w-full rounded-xl p-6 text-base">
+                Log In
+              </Button>
             </Link>
 
             <Link href={'/register'} className="w-1/2">
-              <Button variant={'secondary'} className="w-full p-6 text-lg">
+              <Button
+                variant={'secondary'}
+                className="w-full rounded-xl p-6 text-base"
+              >
                 Register
               </Button>
             </Link>
@@ -129,7 +135,12 @@ export default async function Home() {
         </section>
       </div>
 
-      <footer> </footer>
+      {/* <footer className="flex h-16 w-full">
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <title>GitHub</title>
+          <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+        </svg>
+      </footer> */}
     </>
   );
 }
