559d6ff23c571d6ac0a29be3c9068fc9c52f5174
Noel <ndalfaro333@gmail.com>
2024-12-20 23:49:14 -0600

    updgraded next dependency, added a custom select bg color and changed the prop type definition for the params in player page to be inline

--- app/(main)/players/[id]/page.tsx
+++ app/(main)/players/[id]/page.tsx
@@ -1,57 +1,3 @@
-// import { fetchPlayerDataByID } from '@/app/(main)/lib/data';
-// import { PlayerDetailsStatic } from '@/components/player-detail-static';
-// import { PlayerCardSkeleton } from '@/components/skeletons';
-// import { PlayerStatsChart } from '@/components/player-stats-chart';
-// import { Player } from '@/app/(main)/lib/definitions';
-// import PlayerNews from '@/components/player-news';
-// import PlayerAiSummary from '@/components/player-ai-summary';
-// import PlayerActionBar from '@/components/player-action-bar';
-// type Props = {
-//   params: Promise<{ id: string }>;
-// };
-
-// export const dynamic = 'force-dynamic';
-
-// export default async function PlayerDetailsPage({ params }: Props) {
-//   const { id } = await params;
-//   // Validate ID is a number
-//   const playerId = Number(id);
-
-//   if (isNaN(playerId)) {
-//     return (
-//       <div className="flex flex-col content-center justify-center">
-//         <h1 className="text-4xl font-bold">Invalid Player ID 🫠</h1>
-//       </div>
-//     );
-//   }
-
-//   const player: Player | null = await fetchPlayerDataByID(playerId);
-
-//   if (!player) {
-//     return (
-//       <div className="flex flex-col content-center justify-center">
-//         <h1 className="text-4xl font-bold">
-//           Technical Foul! Player not found. 🫠
-//         </h1>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <>
-//       <div className="grid h-full w-full gap-2 md:max-h-max md:grid-cols-8 md:grid-rows-[350px_1fr_300px] lg:grid-rows-[350px_1fr_250px]">
-//         <PlayerDetailsStatic player={player} />
-//         <PlayerStatsChart stats={player.stats ?? []} />
-//         <PlayerActionBar />
-//         {/* <div className="mb-3 flex w-full flex-wrap gap-2 md:flex-nowrap"> */}
-//         <PlayerNews />
-//         <PlayerAiSummary />
-//         {/* </div> */}
-//       </div>
-//     </>
-//   );
-// }
-
 import { Suspense } from 'react';
 import {
   PlayerDetailsStaticSkeleton,
@@ -68,11 +14,11 @@ import PlayerNews from '@/components/player-news';
 import PlayerAiSummary from '@/components/player-ai-summary';
 import PlayerActionBar from '@/components/player-action-bar';
 
-type Props = {
-  params: Promise<{ id: string }>;
-};
-
-export default async function PlayerDetailsPage({ params }: Props) {
+export default async function PlayerDetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = await params;
   const playerId = Number(id);
 
