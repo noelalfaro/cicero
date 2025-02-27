816c0ba5893527e483d5dfe634ce5eb4d3f75330
Noel <ndalfaro333@gmail.com>
2025-02-27 00:34:39 -0600

    fix: Adjusted path for global css file in onboarding layout

--- app/onboarding/layout.tsx
+++ app/onboarding/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '../../globals.css';
+import '../globals.css';
 import { ThemeProvider } from '@/components/theme/theme-provider';
 import { ViewTransitions } from 'next-view-transitions';
 import { AuthProvider } from '@/components/providers/auth-provider';
